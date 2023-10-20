d9b834002d7e650c90f8c21c85f61542d1fb1b6d
CremerAlbiero <raphael.cremer@hotmail.com>
2023-10-20 17:30:19 -0300

    print function

---
+++ Day 01. Variables to manage Data/01_print.py
@@ -0,0 +1,6 @@
+print('''Day 1 - Python Print Function
+The function is declared like this:
+print('what to print')''')
+
+
+#in python and other programming languages, we can alternate the 'single' and "double" cote in order to give it the same function.
---
+++ Day 01. Variables to manage Data/02_print.py
@@ -0,0 +1,6 @@
+print("First Line\nSecond Line")
+
+#concatenate strings (into an single string):
+print("Hello"+" "+"Guy")
+
+#string concatenation it's simply taking those separate strings of characters and merging them into one.
