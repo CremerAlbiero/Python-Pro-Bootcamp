03a078b9103419eb676c71f45015158a68e5d06b
CremerAlbiero <raphael.cremer@hotmail.com>
2023-10-21 23:59:02 -0300

    Data Types

--- Day 02. Understanding Data Types and Manipulate Strings/dataTypes.py
+++ Day 02. Understanding Data Types and Manipulate Strings/dataTypes.py
@@ -1,2 +1,20 @@
-#you can't use len for int data types.
 #Integers, Float, booleans
+
+print("Hello"[0:5])
+# this method of pulling out a particular element for a string it's called "Subscripting": "string"[0:6].
+print("123" + "345")
+# as long as the data it's within a double quote it's treated like String by the computer.
+
+# Integers
+print(123 + 345)
+
+# Large Integers use _ (just to visualize it more easily). The computer will save it as "Int".
+large_num = 579_456_789_345
+print(large_num)
+
+#Float
+pi_number = 3.14159
+
+#Boolean
+true_value = True
+false_value = False
