94651759a358ced8e2c7f4290ed70a4a0f0591cf
CremerAlbiero <raphael.cremer@hotmail.com>
2023-10-30 02:22:58 -0300

    Day 6: Functions and while loops

---
+++ Day 06. Functions and Karel/02_indentation.py
@@ -0,0 +1,6 @@
+#Pep 8 recommend to use 4 spaces to indent. When you hit tap once, in python, it'll insert 4 spaces.
+
+def my_function():
+    print("Hello")
+my_function()
+# https://peps.python.org/pep-0008/
---
+++ Day 06. Functions and Karel/03_while-loops.py
@@ -0,0 +1,8 @@
+# The loop that will continue going while a condition is true.
+
+number_of_steps = 6
+while number_of_steps > 0:
+    number_of_steps -= 1
+    print(number_of_steps)
+
+#loops exercises: reeborg's world
