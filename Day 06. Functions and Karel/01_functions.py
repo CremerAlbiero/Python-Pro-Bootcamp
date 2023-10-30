7d7829a8facd7098f66b5ccf4b404d422a623811
CremerAlbiero <raphael.cremer@hotmail.com>
2023-10-29 23:54:29 -0300

    Day 6: Functions

---
+++ Day 06. Functions and Karel/01_functions.py
@@ -0,0 +1,16 @@
+# python already has built-in functions. But it's also possible to create specific functions.
+#function().  len(), print()... all are functions.
+
+#defining a function
+def my_function():
+    print("Hello")
+    print("Bye")
+
+#execute the function, line by line.
+my_function()
+
+
+#Defining Functions():
+  # Do this
+  # Then do thus
+  # Finally do this
