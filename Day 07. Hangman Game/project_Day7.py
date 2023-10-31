9701791e36c356441158b0cdee0864fe3bb51067
CremerAlbiero <raphael.cremer@hotmail.com>
2023-10-31 12:06:29 -0300

    Day 7 Project: hangman game

---
+++ Day 07. Hangman Game/project_Day7.py
@@ -0,0 +1,48 @@
+from random import choice
+from words_lists import *
+
+categories = [soccer_teams, holidays, professions, sports, weather, planets, countries, fruits, animals]
+selected_words = []
+
+# choosing randomly an word from "words_list.py".
+for category in categories:
+    random_words = choice(category)
+    selected_words.append(random_words)
+word_selected = choice(selected_words)
+word_lenght = len(word_selected)
+
+
+# putting all the letters in word_selected inside a list.
+word_game = []
+for letters in word_selected:
+    word_game.append(letters)
+
+display_game = []
+for letter in word_selected:
+    display_game.append(" _ ")
+
+# user attempts
+user_attempts = 7
+if word_lenght >= 7:
+    user_attempts = word_lenght + 2 
+print(f"The word has {word_lenght}.\nYou'll have {user_attempts} attempts in total.")
+print(display_game)
+
+while word_game != display_game and user_attempts > 0:
+    user_guess = input("Guess a letter: ").lower()
+    if user_guess in word_game:
+        for i in range(word_lenght):
+            letter = word_game[i]
+            if user_guess == letter:
+                display_game[i] = letter
+            
+        print(" ".join(display_game))
+    else:
+        user_attempts -= 1
+        print(f"Wrong. You have {user_attempts} attempts left.")
+        print(" ".join(display_game))
+
+if word_game == display_game:
+    print("You won!")
+else:
+    print("Game over.")
---
+++ Day 07. Hangman Game/words_lists.py
@@ -0,0 +1,22 @@
+animals = ["dog", "cat", "elephant", "giraffe", "lion", "tiger", "zebra", "kangaroo", "penguin", "dolphin"]
+
+fruits = ["apple", "banana", "strawberry", "watermelon", "pineapple", "kiwi", "orange", "blueberry", "grape", "lemon"]
+
+countries = ["brazil", "united states", "canada", "france", "germany", "japan", "australia", "italy", "india", "china"]
+
+planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"]
+
+weather = ["sunny", "rainy", "windy", "cloudy", "snow", "thunder", "fog", "tornado", "hurricane", "rainbow"]
+
+sports = ["soccer", "basketball", "tennis", "swimming", "golf", "cycling", "volleyball", "baseball", "hockey", "skiing"]
+
+professions = ["doctor", "teacher", "engineer", "musician", "chef", "firefighter", "police", "artist", "astronaut", "scientist"]
+
+holidays = ["christmas", "halloween", "thanksgiving", "valentine", "easter", "new year", "independence", "labor day"]
+
+soccer_teams = [
+    "real madrid", "barcelona", "manchester united", "liverpool", "bayern munich", "paris saint-germain", "juventus",
+    "ac milan", "chelsea", "arsenal", "borussia dortmund", "atletico madrid", "inter milan", "manchester city",
+    "tottenham", "boca juniors", "river plate", "flamengo", "palmeiras", "santos", "sao paulo",
+    "corinthians", "fluminense", "gremio", "internacional", "ajax", "benfica"
+]
